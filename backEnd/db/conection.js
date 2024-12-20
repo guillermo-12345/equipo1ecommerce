4e93d987a6b2e92112e8ed682c94a3d1629e1f31
Alex <alexbecerra456@gmail.com>
2024-12-20 12:34:31 -0300

    conection mod

--- backEnd/db/conection.js
+++ backEnd/db/conection.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql2');
-require('dotenv').config();
+
+
+require('dotenv').config({ path: './.env' });
+
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST || "mysql",
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "MordeDB",
+    database: process.env.DB_NAME || "dbtp5"
    
 };
 
