f91cb4104c3bf3914bcd688fd02c08cb68557f1f
Alex <alexbecerra456@gmail.com>
2025-08-24 01:02:47 -0300

    nuevo firebase

--- frontEnd/src/components/config/firebase.jsx
+++ frontEnd/src/components/config/firebase.jsx
@@ -5,15 +5,23 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyByMb5GwMMb1_T40lx-V5bWx1Y4pfOAw7Q",
+//   authDomain: "ecommerce-6d9e2.firebaseapp.com",
+//   projectId: "ecommerce-6d9e2",
+//   storageBucket: "ecommerce-6d9e2.firebasestorage.com",
+//   messagingSenderId: "462392931099",
+//   appId: "1:462392931099:web:5894291dec9a8576df59be"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyByMb5GwMMb1_T40lx-V5bWx1Y4pfOAw7Q",
-  authDomain: "ecommerce-6d9e2.firebaseapp.com",
-  projectId: "ecommerce-6d9e2",
-  storageBucket: "ecommerce-6d9e2.firebasestorage.com",
-  messagingSenderId: "462392931099",
-  appId: "1:462392931099:web:5894291dec9a8576df59be"
+  apiKey: "AIzaSyCW82Vtr3WOHCgLM1cKADuZw0Vk4CluMSo",
+  authDomain: "tp5ecommerce.firebaseapp.com",
+  projectId: "tp5ecommerce",
+  storageBucket: "tp5ecommerce.firebasestorage.app",
+  messagingSenderId: "587628775178",
+  appId: "1:587628775178:web:02635300d9fc78e83e1cf3",
+  measurementId: "G-ENY1BZ0LLR"
 };
-
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 
