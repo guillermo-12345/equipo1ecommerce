c1aeb4d84af953ad7e04432da86532a7f5bdb16d
Alex <alexbecerra456@gmail.com>
2025-09-25 18:48:28 -0300

    firebase storageBucket

--- frontEnd/src/components/config/firebase.jsx
+++ frontEnd/src/components/config/firebase.jsx
@@ -5,19 +5,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyByMb5GwMMb1_T40lx-V5bWx1Y4pfOAw7Q",
-//   authDomain: "ecommerce-6d9e2.firebaseapp.com",
-//   projectId: "ecommerce-6d9e2",
-//   storageBucket: "ecommerce-6d9e2.firebasestorage.com",
-//   messagingSenderId: "462392931099",
-//   appId: "1:462392931099:web:5894291dec9a8576df59be"
-// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyCW82Vtr3WOHCgLM1cKADuZw0Vk4CluMSo",
   authDomain: "tp5ecommerce.firebaseapp.com",
   projectId: "tp5ecommerce",
-  storageBucket: "tp5ecommerce.firebasestorage.app",
+  storageBucket: "tp5ecommerce.appspot.com",
   messagingSenderId: "587628775178",
   appId: "1:587628775178:web:02635300d9fc78e83e1cf3",
   measurementId: "G-ENY1BZ0LLR"
@@ -30,3 +23,11 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export default app;
+// const firebaseConfig = {
+//   apiKey: "AIzaSyByMb5GwMMb1_T40lx-V5bWx1Y4pfOAw7Q",
+//   authDomain: "ecommerce-6d9e2.firebaseapp.com",
+//   projectId: "ecommerce-6d9e2",
+//   storageBucket: "ecommerce-6d9e2.firebasestorage.com",
+//   messagingSenderId: "462392931099",
+//   appId: "1:462392931099:web:5894291dec9a8576df59be"
+// };
